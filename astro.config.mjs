38d261f87a79ca083a9625968d4cf661aa94c8bd
Huzaifa Shaikh <huzaifashaikh9923@gmail.com>
2026-08-01 13:12:29 +0530

    Update astro.config.mjs - 100% Sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
   trailingSlash: 'never',
   vite: {
     plugins: [
